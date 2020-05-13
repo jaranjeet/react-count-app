27cded8729d1bda7267c83bc858ada57f7935044
Ranjeet Jagtap <ranjeet.jagtap43@gmail.com>
2020-05-13 17:31:20 +0530

    Added navbar, app. Lifted the state up to app

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,32 +1,24 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    value: this.props.value
-  }
-
-  handleIncrement = () => {
-    this.setState({value: this.state.value + 1});
-  }
-
   render() {
     return (
       <div>
         <span className={ this.getBadgeClasses() }>{ this.formatValue() }</span>
-        <button onClick={ this.handleIncrement } className="btn btn-secondary btn-sm">Increment</button>
-        <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+        <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-"
-    classes += (this.state.count === 0) ? 'warning' : 'primary'
+    classes += (this.props.counter.value === 0) ? 'warning' : 'primary'
     return classes;
   }
 
   formatValue() {
-    const value = this.state.value
+    const { value } = this.props.counter;
     return (value === 0) ? 'Zero' : value;
   }
 }
